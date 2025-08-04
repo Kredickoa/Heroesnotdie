f7ad6ee8762d49ffecb9b1bb05bf9e6318621873
Kredickoa <141122946+Kredickoa@users.noreply.github.com>
2025-08-04 19:57:57 +0200

    fast fix

--- src/commands/activity/pulse-check.py
+++ src/commands/activity/pulse-check.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from database import get_database
 from datetime import datetime, timedelta
 
 class PulseCheck(commands.Cog):
--- src/commands/activity/pulse-disable.py
+++ src/commands/activity/pulse-disable.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from database import get_database
 
 class PulseDisable(commands.Cog):
     def __init__(self, bot):
