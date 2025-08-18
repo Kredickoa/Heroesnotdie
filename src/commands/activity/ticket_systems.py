bc14902a9fc5334fcb7502db2bc8f6d15450e1ec
Kredickoa <141122946+Kredickoa@users.noreply.github.com>
2025-08-18 10:12:02 +0200

    f2

--- src/commands/activity/ticket_systems.py
+++ src/commands/activity/ticket_systems.py
@@ -260,7 +260,7 @@ class TicketTypeSelect(discord.ui.Select):
                 title=f"{config['name']}",
                 description=f"**Користувач:** {interaction.user.mention}\n**Створено:** <t:{int(datetime.now().timestamp())}:F>",
                 color=0x2b2d31,
-                timestamp=datetime.now(в)
+                timestamp=datetime.now()
             )
             
             if role_id:
